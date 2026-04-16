561c6456e9c869c43b8d96d4b4de102100c96b77
Abhijeetchattopadhyay <152309477+Abhijeetchattopadhyay@users.noreply.github.com>
2026-04-16 14:51:09 +0530

    Time: 18 ms (6.67%), Space: 25.1 MB (5.12%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     bool isAnagram(string a,string b){
-        int n1=a.size();
-        int n2=b.size();
-        if(n1!=n2){
+        if(a.size()!=b.size()){
             return false;
         }
         unordered_map<char,int>m1;
         unordered_map<char,int>m2;
-        for(int i=0;i<n1;i++){
-             m1[a[i]]++;
+        for(int i=0;i<a.size();i++){
+            m1[a[i]]++;
         }
-        for(int i=0;i<n1;i++){
-             m2[b[i]]++;
+        for(int i=0;i<b.size();i++){
+            m2[b[i]]++;
         }
-        if(m1==m2){
+        if(m1==m2) {
             return true;
-        }else{
-            return false;
+        }    
+        else{ 
+           return false;
         }
+        
     }
     vector<string> removeAnagrams(vector<string>& words) {
-        unordered_map<string,vector<string>>result;
-        vector<string>ans;
         int n=words.size();
+        vector<string>ans;
         string s=words[0];
-        ans.push_back(s);
+        ans.push_back(words[0]);
         for(int i=1;i<n;i++){
-           string s1=words[i];
-           if(!isAnagram(s,s1)){
-               ans.push_back(words[i]);
-               s=words[i];
-           }
+          string s1=words[i];
+          if(!isAnagram(s,s1)){
+            ans.push_back(words[i]);
+          }
+         s=words[i];
         }
         return ans;
     }
