8f9ee22a2e925f0f176b7aef4a8327c993a54b36
Abhijeetchattopadhyay <152309477+Abhijeetchattopadhyay@users.noreply.github.com>
2026-05-07 00:53:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,45 +1,33 @@
 class MyQueue {
 public:
-stack<int>input;
-stack<int>output;
-int peakelement=-1;
+    stack<int>input;
+    stack<int>output;
     MyQueue() {
         
     }
     
     void push(int x) {
-        if(input.empty()){
-            peakelement=x;
-        }
         input.push(x);
     }
-    // amortizedo(1)
+    
     int pop() {
-       if(output.empty()){
-        //input->ouput 
-        while(!input.empty()){
-           output.push(input.top());
-           input.pop();
-       }
-    }
-       int val=output.top();
-       output.pop();
-       return val;
-        
+        int del=output.top();
+        output.pop();
+        return del;
     }
     
     int peek() {
         if(output.empty()){
-            return peakelement;
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
         }
         return output.top();
     }
     
     bool empty() {
-        if(input.empty() && output.empty()){
-            return true;
-        }
-        return false;
+        return input.empty() && output.empty();
     }
 };
 
