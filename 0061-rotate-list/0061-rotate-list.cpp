d095c950b9ca1accbf5ddc09eb0cc7087e568867
Abhijeetchattopadhyay <152309477+Abhijeetchattopadhyay@users.noreply.github.com>
2026-05-08 23:51:17 +0530

    Time: 0 ms (100%), Space: 16.2 MB (94.42%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,31 +11,34 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL){
-            return head;    
-        }
-        int count =0;
-        ListNode* temp = head;
-        while(temp != NULL){
-            count++;
-            temp = temp->next;
-        }
-        k=k%count;
-        if(k==0) return head;
-        count = count - k;
+       if(head==NULL || head->next==NULL || k==0){
+        return head;
+       }
+       int count=0;
+       ListNode*temp=head;
+       while(temp!=NULL){
+          count++;
+          temp=temp->next;
+       } 
+       k=k%count;
+       if(k==0){
+        return head;
+       }
+       int rotate=count-k;
+       
+       ListNode*curr=head;
+       ListNode*prev=NULL;
+       while(rotate--){
+          prev=curr;
+          curr=curr->next;
+       }
+       prev->next=NULL;
+       ListNode*futt=curr;
+       while(curr->next!=NULL){
+        curr=curr->next;
+       }
+       curr->next=head;
+       return futt;
 
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        while(count--){
-            prev = curr;
-            curr = curr->next;
-        }
-        prev ->next = NULL;
-        ListNode* tail = curr;
-        while(tail->next != NULL){
-            tail = tail->next;
-        }
-        tail->next = head;
-        return curr;
     }
 };
