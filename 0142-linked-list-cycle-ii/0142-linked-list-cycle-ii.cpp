f28b5e15fa163cf5a25a92d7e7cf79f2c950580e
Abhijeetchattopadhyay <152309477+Abhijeetchattopadhyay@users.noreply.github.com>
2026-05-24 01:35:41 +0530

    Time: 0 ms (100%), Space: 11.4 MB (22.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,29 +6,22 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- // initialize slow and fast at head;
- // move slow by 1and fast by 2 steps untill they intersect
- // then intersection point should be kept safe;
- // then move slow back to head
- // then move slow by 1 and intersectioon by 1
- // wherever they intersect is the starting point
-
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode *slow=head;
-        ListNode *fast=head;
+        ListNode*slow=head;
+        ListNode*fast=head;
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-
             if(slow==fast){
                 slow=head;
-              while(slow!=fast){
+            
+            while(slow!=fast){
                 slow=slow->next;
                 fast=fast->next;
             }
-            return fast;
+            return slow;
             }
         }
         return NULL;
