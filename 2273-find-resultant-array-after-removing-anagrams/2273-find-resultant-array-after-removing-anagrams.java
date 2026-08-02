abcf5beb6d031e9bc81a37fff088d481e630354e
Abhijeetchattopadhyay <152309477+Abhijeetchattopadhyay@users.noreply.github.com>
2026-08-02 15:56:04 +0530

    Time: 8 ms (10.52%), Space: 47.5 MB (5.43%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean anagram(String s1, String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        HashMap<Character,Integer>mp1 = new HashMap<>();
+        HashMap<Character,Integer>mp2 = new HashMap<>();
+
+        for(int i=0;i<s1.length();i++){
+            mp1.put(s1.charAt(i),mp1.getOrDefault(s1.charAt(i),0)+1);
+        }
+        for(int i=0;i<s2.length();i++){
+            mp2.put(s2.charAt(i),mp2.getOrDefault(s2.charAt(i),0)+1);
+        }
+        return mp1.equals(mp2);
+    }
+    public List<String> removeAnagrams(String[] words) {
+        int n=words.length;
+        ArrayList<String>result = new ArrayList<>();
+        result.add(words[0]);
+        for(int i=1;i<n;i++){
+            String words1="";
+            if(!anagram(words[i],words[i-1])){
+                result.add(words[i]);
+            }
+            words1 = words[i];
+
+
+        }
+        return result;
+    }
+}
