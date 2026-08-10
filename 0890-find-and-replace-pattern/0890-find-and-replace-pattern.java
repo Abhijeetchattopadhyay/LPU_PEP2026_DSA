3e0dd9a4c77c1bc8888453983c5418df5ff9a71f
Abhijeetchattopadhyay <152309477+Abhijeetchattopadhyay@users.noreply.github.com>
2026-08-10 22:10:14 +0530

    Time: 2 ms (34.37%), Space: 43.8 MB (61.71%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -2,28 +2,23 @@ class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         ArrayList<String>result = new ArrayList<>();
         for(int i=0;i<words.length;i++){
-            HashMap<Character,Character>mp1=new HashMap<>();
+            HashMap<Character,Character>mp1 = new HashMap<>();
             HashMap<Character,Character>mp2 = new HashMap<>();
-            boolean valid=true;
-            String currentword = words[i];
-            for(int j=0;j<currentword.length();j++){
-                char ch=currentword.charAt(j);
+            boolean flag = true;
+            String ans=words[i];
+            for(int j=0;j<ans.length();j++){
+                char ch=ans.charAt(j);
                 char th=pattern.charAt(j);
-                if(mp1.containsKey(ch) && mp1.get(ch)!=th || mp2.containsKey(th) && mp2.get(th)!=ch){
-                if(valid){
-                    valid=false;
+                if(mp1.containsKey(ch) && mp1.get(ch)!=th || mp2.containsKey(th)&& mp2.get(th)!=ch){
+                    flag = false;
                     break;
                 }
-                }
-                
-              
-               mp1.put(ch,th);
-               mp2.put(th,ch);
+                mp1.put(ch,th);
+                mp2.put(th,ch);
             }
-            if(valid==true){
-            result.add(currentword); 
+            if(flag){
+                result.add(ans);
             }
-            
         }
         return result;
     }
